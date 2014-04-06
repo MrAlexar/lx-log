01f0f644e518fec412e9ee31be32576d371e86f8
Alexar <alexarpen@gmail.com>
2014-04-06 22:01:05 +1000

    writes teh file

--- lx-log.sh
+++ lx-log.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/php
 <?php
-echo 'salam';
+$filename = 'DailyLog.txt';
+$filedir = '/Users/alexarpen/Documents';
+
+$filepath = sprintf('%s/%s', $filedir, $filename);
+
+$aInput = $_SERVER['argv'];
+array_shift($aInput);
+$sInput = implode("\s", $aInput);
+$sEntry = sprintf("%s\t%s", date('Y-m-d H:i:s'), $sInput);
+$content = implode("\n", array(
+	file_get_contents($filepath)
+	, $sEntry
+));
+
+file_put_contents($filepath, $content);
+
+echo sprintf("%s\n", $sEntry);
