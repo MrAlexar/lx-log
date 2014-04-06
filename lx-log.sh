bec9360974df3203ae9f1b286757d639ca6434a9
Alexar <alexarpen@gmail.com>
2014-04-06 22:16:42 +1000

    Code cleanup. PS: Using the magix of 'switch' ;)

--- lx-log.sh
+++ lx-log.sh
@@ -1,19 +1,53 @@
 #!/usr/bin/php
 <?php
-$filename = 'DailyLog.txt';
-$filedir = '/Users/alexarpen/Documents';
 
-$filepath = sprintf('%s/%s', $filedir, $filename);
 
-$aInput = $_SERVER['argv'];
-array_shift($aInput);
-$sInput = implode(" ", $aInput);
-$sEntry = sprintf("%s\t%s", date('Y-m-d H:i:s'), $sInput);
-$content = implode("\n", array(
-	file_get_contents($filepath)
-	, $sEntry
-));
+$sInput = getInput();
 
-file_put_contents($filepath, $content);
+switch ($sInput) {
+	case '--help':
+		showHelp();
+		break;
+	default:
+		addLog($sInput);
+	case '':
+		showLast();
+}
 
-echo sprintf("%s\n", $sEntry);
+function getInput() {
+	$aInput = $_SERVER['argv'];
+	array_shift($aInput);
+	return implode(" ", $aInput);
+}
+
+function addLog($sLog) {
+	$sEntry = sprintf("%s\t%s", date('Y-m-d H:i:s'), $sLog);
+	$filepath = getFilename();
+	$content = implode("\n", array(
+		file_get_contents($filepath)
+		, $sEntry
+	));
+	file_put_contents($filepath, $content);
+}
+
+function showHelp() {
+	writeOutput('lx-log [any [combination [of [words [...]]]]]');
+}
+
+function writeOutput($sText) {
+	echo $sText . "\n";
+	exit;
+}
+
+function getFilename() {
+	$filename = 'DailyLog.txt';
+	$filedir = '/Users/alexarpen/Documents';
+	return sprintf('%s/%s', $filedir, $filename);	
+}
+
+function showLast() {
+	$aLines = explode("\n", file_get_contents(getFilename()));
+	$sLast = reset(array_reverse($aLines));
+	writeOutput($sLast);
+}
+
