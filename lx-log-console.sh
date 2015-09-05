fb06657bbf58ca088c6eafda4d8338b5e1a529d7
Alexar <hi@alexar.me>
2015-09-05 10:02:29 +0430

    Stop simple return to reveal the last comment.

--- lx-log-console.sh
+++ lx-log-console.sh
@@ -2,6 +2,11 @@ echo "Start logging. Press Ctrl+C to stop...";
 until false;
 	do 
 		read -s cmd;
-		lx-log $cmd;
-		echo "Logged " ${#cmd} " character(s).";
+		len=${#cmd};
+		if [ $len != 0 ]; then
+			lx-log $cmd;
+			echo "Logged " ${#cmd} " character(s).";
+		else
+			echo "Null";
+		fi
 done
